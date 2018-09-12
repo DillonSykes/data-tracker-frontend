f773814a17ff199aa7d9f6108d5df0b45c62d607
Dillon Sykes <dsykes321@gmail.com>
2018-09-12 19:46:52 -0400

    adds Auth support and login works.

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Router} from '@angular/router';
+import * as luxon from 'luxon';
+// import { JwtHelperService } from '@auth0/angular-jwt';
 @Injectable({
   providedIn: 'root'
 })
 
 export class ApiService {
   private headers =  new HttpHeaders({'Content-Type': 'application/json'});
-
+  public loggedInStatus = false;
   constructor(private http: HttpClient, private router: Router) { }
-
-
+  // public isAuthenticated(): boolean {
+  //
+  //   const token =  localStorage.getItem('token');
+  //   return !this.jwtHelper.isTokenExpired(token);
+  // }
   getUserCredentials(username: string, password: string) {
     // const headers = new HttpHeaders({'Content-Type':'application/json; charset=utf-8'});
     this.headers.append('Access-Control-Allow-Origin', '*');
@@ -21,10 +26,40 @@ export class ApiService {
       const data: any = res;
       if (data.auth === true) {
         this.router.navigateByUrl('/home');
-      }
-      else {
+        this.loggedInStatus = this.isLoggedIn();
+        console.log(data);
 
+      } else {
+        window.alert('Loggin Failed');
       }
     });
+  }
+  getLoginStatus() {
+    return this.loggedInStatus;
+  }
+  private setSession(authResult) {
+    const expiresAt = luxon.add(authResult.expiresIn, 'second');
+
+    localStorage.setItem('id_token', authResult.token);
+    localStorage.setItem('expires_at', JSON.stringify(expiresAt.valueOf()) );
+  }
+
+  logout() {
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
+  }
+
+  public isLoggedIn() {
+    return luxon.isBefore(this.getExpiration());
+  }
+
+  isLoggedOut() {
+    return !this.isLoggedIn();
+  }
+
+  getExpiration() {
+    const expiration = localStorage.getItem('expires_at');
+    const expiresAt = JSON.parse(expiration);
+    return luxon(expiresAt);
   }
 }
--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
-
+import { ApiService } from './api.service';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor(private api: ApiService) { }
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+    return this.api.getLoginStatus();
   }
 }
