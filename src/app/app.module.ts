4d6a001fe772fd771d2dedaf7fcf1e8656a3fdf4
Dillon Sykes <dsykes321@gmail.com>
2018-12-19 22:00:40 -0500

    removes Goal import for app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,7 @@ import { CheckBoxComponent } from "./components/check-box/check-box.component";
 import { PersonFormComponent } from "./components/person-form/person-form.component";
 import { ChildrenComponent } from "./components/children/children.component";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
-import { Goals, Person } from "./models";
+import { Person } from "./models";
 import { GrandchildrenComponent } from "./components/grandchildren/grandchildren.component";
 import { CareTakerComponent } from "./components/care-taker/care-taker.component";
 import { StoreModule } from "@ngrx/store";
