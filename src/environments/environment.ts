7ecdd13e4025dde72f8cf17e682de60fbec6fdde
Dillon Sykes <dsykes321@gmail.com>
2018-12-21 11:57:02 -0500

    addsflags

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   API_ENDPOINT: "https://evr5af1fha.execute-api.us-east-1.amazonaws.com/dev",
   LOCAL_HOST: "http://localhost:3000"
 };
