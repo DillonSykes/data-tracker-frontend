d3274089704ae8d04f6899f359c10609ef6c1ad1
Dillon Sykes <dsykes321@gmail.com>
2019-03-24 00:10:53 -0400

    inital commit.

---
+++ src/app/client-view/rental-view/rental-view.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-rental-view",
+  template: ``,
+})
+export class RentalViewComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
