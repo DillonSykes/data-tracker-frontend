92e0b4750556120ef154ba331859ce64c79af7be
Dillon Sykes <dsykes321@gmail.com>
2019-04-26 00:31:13 -0400

    feat: rental view (#17)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -42,6 +42,7 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { ToastrModule } from "ngx-toastr";
 import { InfoNavBarComponent } from "./client-view/info-nav-bar/info-nav-bar.component";
 import { HomeViewComponent } from "./client-view/home-view/home-view.component";
+import { RentalViewComponent } from "./client-view/rental-view/rental-view.component";
 
 @NgModule({
   declarations: [
@@ -71,6 +72,7 @@ import { HomeViewComponent } from "./client-view/home-view/home-view.component";
     ChildrenViewComponent,
     InfoNavBarComponent,
     HomeViewComponent,
+    RentalViewComponent,
   ],
   imports: [
     CommonModule,
